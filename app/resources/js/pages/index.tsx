aaa07aa2895e38a20fa1368573a91fcd5a3cc0a4
yurio <monochro4s@gmail.com>
2025-11-07 14:19:14 +0900

    [ update ] トップページのCard作成

--- app/resources/js/pages/index.tsx
+++ app/resources/js/pages/index.tsx
@@ -1,6 +1,12 @@
-import { register } from '@/routes';
-import { type SharedData } from '@/types';
-import { Head, Link, usePage } from '@inertiajs/react';
+import {
+    HomeCard,
+    HomeCardHeader,
+    HomeCardFooter,
+    HomeCardTitle,
+    HomeCardDescription,
+    HomeCardContent
+} from '@/components/ui/home-card';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Index(
    ) {
@@ -15,8 +21,8 @@ export default function Index(
                 />
             </Head>
             <div className="flex min-h-screen flex-col items-center bg-[#efefef] text-[#1b1b18] dark:bg-[#0a0a0a]">
-                <header className="fixed mb-6 w-full text-lg p-2 bg-[#494544] text-[#FDFDFC]">
-                    <div className="mx-auto items-center lg:max-w-4xl flex justify-between gap-4">
+                <header className="fixed mb-6 w-full text-lg py-2 bg-[#494544] text-[#FDFDFC]">
+                    <div className="mx-auto items-center lg:max-w-4xl flex justify-between gap-4 px-3">
                         <h1>80歳まで続けるサイト</h1>
                         <nav className="justify-end">
                             <Link
@@ -30,11 +36,14 @@ export default function Index(
                         </nav>
                     </div>
                 </header>
-                <div className="flex w-full items-center lg:max-w-4xl max-w-sm pt-16">
+                <div className="flex w-full items-center lg:max-w-4xl pt-16 px-3">
                     <main className="w-full">
-                        <p>80歳になる2065年まで40年続けるサイトです</p>
-                        <h2 className="text-2xl p-1 border-b border-[#0a0a0a]">プロフィール</h2>
-                        <p>不登校、ひきこもり、長期無職、対人恐怖（社交不安障害）、短期離職の繰り返しから、自分の夢を叶える方法を模索しています。</p>
+                        <p className="p-2">80歳になる2065年まで40年続けるサイトです。</p>
+                        <HomeCard>
+
+                        </HomeCard>
+                        <h2 className="text-2xl pt-1 pb-1 border-b border-[#0a0a0a]">プロフィール</h2>
+                        <p className="p-2">不登校、ひきこもり、長期無職、対人恐怖（社交不安障害）、短期離職の繰り返しから、自分の夢を叶える方法を模索しています。</p>
                         <h2 className="text-2xl border-b border-[#c41a30]">読み物</h2>
                     </main>
                 </div>
