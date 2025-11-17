e94594f143404ce3d99346784e00a15d974ab41b
yuri <monochro4s@gmail.com>
2025-11-17 13:32:02 +0900

    [ update ] markdownの要素にclass設定

--- app/resources/js/pages/contents/posts/show.tsx
+++ app/resources/js/pages/contents/posts/show.tsx
@@ -19,6 +19,23 @@ export default function Index({
                 <ReactMarkdown
                     remarkPlugins={[remarkGfm]}
                     rehypePlugins={[rehypeHighlight]}
+                    components={{
+                        h1: ({node, ...props}) => (
+                        <h1 className="text-3xl font-bold mt-8 mb-4" {...props} />
+                        ),
+                        h2: ({node, ...props}) => (
+                        <h2 className="text-2xl font-semibold mt-6 mb-3" {...props} />
+                        ),
+                        p: ({node, ...props}) => (
+                        <p className="text-base leading-7 mb-4" {...props} />
+                        ),
+                        ul: ({node, ...props}) => (
+                        <ul className="list-disc pl-6 mb-4" {...props} />
+                        ),
+                        a: ({node, ...props}) => (
+                        <a className="text-blue-600 underline" {...props} />
+                        ),
+                    }}
                 >
                     {post.body_markdown}
                 </ReactMarkdown>
