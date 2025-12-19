39315ee4d9b2d110d2f7477697e99faf2495b95b
yuri <monochro4s@gmail.com>
2025-12-19 17:47:47 +0900

    [ add ] ひきこもりメモ HTMLからDBにデータ移行

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,6 @@ class DatabaseSeeder extends Seeder
             ]
         );
         $this->call(ComicSeeder::class);
-        $this->call(AdminSeeder::class);
+        $this->call(HikiSeeder::class);
     }
 }
