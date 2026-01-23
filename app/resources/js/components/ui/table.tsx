3cf75489768aff34f1c8ddc712aee9fb66e67744
yuri <monochro4s@gmail.com>
2026-01-23 20:20:30 +0900

    [ fix ] 横スクロール対応 #38

--- app/resources/js/components/ui/table.tsx
+++ app/resources/js/components/ui/table.tsx
@@ -9,7 +9,7 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
     <div
       data-slot="table-container"
-      className="relative"
+      className="relative overflow-x-auto"
     >
       <table
         data-slot="table"
